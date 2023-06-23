5623d30fabcc9f0800562c1b71c6a742e8bf969a
thang_hd <thang-hd@if-vietnam.com>
2023-06-23 16:49:46 +0700

    bk

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -21,32 +21,10 @@ import Switch from '@mui/material/Switch';
 import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
+import axios from 'axios';
 
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
+import ModalPopup from '../../components/Modal';
 
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-];
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -85,31 +63,37 @@ const headCells = [
     id: 'name',
     numeric: false,
     disablePadding: true,
-    label: 'Dessert (100g serving)',
+    label: 'name',
   },
   {
-    id: 'calories',
-    numeric: true,
+    id: 'username',
+    numeric: false,
     disablePadding: false,
-    label: 'Calories',
+    label: 'username',
   },
   {
-    id: 'fat',
-    numeric: true,
+    id: 'email',
+    numeric: false,
     disablePadding: false,
-    label: 'Fat (g)',
+    label: 'email',
   },
   {
-    id: 'carbs',
-    numeric: true,
+    id: 'address',
+    numeric: false,
     disablePadding: false,
-    label: 'Carbs (g)',
+    label: 'address',
   },
   {
-    id: 'protein',
-    numeric: true,
+    id: 'phone',
+    numeric: false,
     disablePadding: false,
-    label: 'Protein (g)',
+    label: 'phone',
+  },
+  {
+    id: 'edit',
+    numeric: false,
+    disablePadding: false,
+    label: '',
   },
 ];
 
@@ -225,6 +209,7 @@ EnhancedTableToolbar.propTypes = {
 };
 
 export default function EnhancedTable() {
+  const [rows, setRows] = React.useState([]);
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState('calories');
   const [selected, setSelected] = React.useState([]);
@@ -267,6 +252,12 @@ export default function EnhancedTable() {
     setSelected(newSelected);
   };
 
+  const handleChangeRow = (id, newRow) => {
+    const newRows = [...rows];
+    newRows[id] = newRow;
+    setRows(newRows);
+  };
+
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
@@ -279,6 +270,14 @@ export default function EnhancedTable() {
   const handleChangeDense = (event) => {
     setDense(event.target.checked);
   };
+  
+  React.useEffect(() =>{
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => {
+      setRows(res.data)
+    })
+    .catch(error => console.log(error))
+  }, []);
 
   const isSelected = (name) => selected.indexOf(name) !== -1;
 
@@ -294,7 +293,6 @@ export default function EnhancedTable() {
       ),
     [order, orderBy, page, rowsPerPage],
   );
-
   return (
     <Box sx={{ width: '100%' }}>
       <Paper sx={{ width: '100%', mb: 2 }}>
@@ -346,10 +344,11 @@ export default function EnhancedTable() {
                     >
                       {row.name}
                     </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell align="left">{row.username}</TableCell>
+                    <TableCell align="left">{row.email}</TableCell>
+                    <TableCell align="left">{`${row.address.street}, ${row.address.suite}`}</TableCell>
+                    <TableCell align="left">{row.phone}</TableCell>
+                    <TableCell align="left"><ModalPopup dataRow={row} rowId={index} onChangeRow={handleChangeRow} /></TableCell>
                   </TableRow>
                 );
               })}
